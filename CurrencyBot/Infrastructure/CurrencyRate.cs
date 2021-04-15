9bd4c27568a1366b74e8b38845fbfc93bd009ba3
Медко Никита <nikita.jove28@gmail.com>
2021-04-15 22:15:15 +0300

    Implementation CurrencyRate

---
+++ CurrencyBot/Infrastructure/CurrencyRate.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CurrencyBot.Infrastructure
+{
+    public class CurrencyRate
+    {
+        public int r030 { get; set; }
+        public string txt { get; set; }
+        public float rate { get; set; }
+        public string cc { get; set; }
+        public string exchangedate { get; set; }
+    }
+}
--- CurrencyBot/NBU.cs
+++ CurrencyBot/NBU.cs
@@ -1,12 +1,25 @@
-﻿using System;
+﻿using CurrencyBot.Infrastructure;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Json;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace CurrencyBot
 {
-    class NBU
+    public class NBU
     {
+        public static async Task<List<CurrencyRate>> GetRatesAsync()
+        {
+            using var httpClient = new HttpClient();
+            return await httpClient.GetFromJsonAsync<List<CurrencyRate>>("https://bank.gov.ua/NBUStatService/v1/statdirectory/exchange?json");
+        }
+        public static async Task<List<CurrencyRate>> GetRatesByDateAsync(string date)
+        {
+            using var httpClient = new HttpClient();
+            return await httpClient.GetFromJsonAsync<List<CurrencyRate>>($"https://bank.gov.ua/NBUStatService/v1/statdirectory/exchange?date={date}&json");
+        }
     }
 }
